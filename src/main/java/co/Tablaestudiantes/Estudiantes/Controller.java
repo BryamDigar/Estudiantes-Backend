bdd8c17330e4f0e13a49bb763de5ad7ba50f4d36
BryamDigar <bryamdiaz1010@gmail.com>
2023-05-06 21:31:52 -0500

    Se filtra la cantidad de caracteres minimo en las variables

--- src/main/java/co/Tablaestudiantes/Estudiantes/Controller.java
+++ src/main/java/co/Tablaestudiantes/Estudiantes/Controller.java
@@ -9,34 +9,41 @@ import java.util.List;
 public class Controller {
     List<Estudiante> estudianteList;
 
-    public Controller(){
+    public Controller() {
         this.estudianteList = new ArrayList<>();
-        estudianteList.add(new Estudiante("Daniel",1,3,"Ingenieria"));
-        estudianteList.add(new Estudiante("Samuel",2,5,"Comunicacion"));
-        estudianteList.add(new Estudiante("Andrea",3,4,"Derecho"));
-        estudianteList.add(new Estudiante("Fulana",4,7,"Ingenieria"));
-        estudianteList.add(new Estudiante("Jaime",5,3,"Ingenieria"));
+        estudianteList.add(new Estudiante("Daniel", 100000, 3, "Ingenieria"));
+        estudianteList.add(new Estudiante("Samuel", 100002, 5, "Comunicacion"));
+        estudianteList.add(new Estudiante("Andrea", 100003, 4, "Derecho"));
+        estudianteList.add(new Estudiante("Fulana", 100004, 7, "Ingenieria"));
+        estudianteList.add(new Estudiante("Jaime", 100005, 3, "Ingenieria"));
     }
 
     @GetMapping(path = "/estudiantes/todos")
-    public List<Estudiante> obtenerEstudiante(){
+    public List<Estudiante> obtenerEstudiante() {
         return estudianteList;
     }
+
     @GetMapping(path = "")
-    public String hola(){
+    public String hola() {
         return "Hola";
     }
 
 
     @PostMapping(path = "/estudiante/crear")
     public String crearEstudiante(@RequestBody Estudiante estudiante) {
-        estudiante.setId(filtrarID());
-        estudianteList.add(estudiante);
-        return "Estudiante ingresado correctamente";
+        String estado = "";
+        if ((String.valueOf(estudiante.getSemestre()).length() >= 1) && (String.valueOf(estudiante.getFacultad()).length() >= 1) && (String.valueOf(estudiante.getNombre()).length() >= 3)) {
+            estudiante.setId(filtrarID());
+            estudianteList.add(estudiante);
+            estado = "Estudiante ingresado correctamente";
+        } else {
+            estado = "Fallo en los datos";
+        }
+        return estado;
     }
 
     public int filtrarID() {
-        int id = 0;
+        int id = 100000;
         for (Estudiante estudiante : estudianteList) {
             if (estudiante.getId() > id) {
                 id = estudiante.getId();
--- src/main/java/co/Tablaestudiantes/Estudiantes/EstudiantesApplication.java
+++ src/main/java/co/Tablaestudiantes/Estudiantes/EstudiantesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EstudiantesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EstudiantesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EstudiantesApplication.class, args);
+    }
 
 }
